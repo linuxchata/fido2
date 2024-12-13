d579f55964ad5cd069a909987d7ded4c2d44ca5b
linuxchata <linuxchata@gmail.com>
2024-12-13 16:56:45 +0100

    Add AuthenticatorAttachment enum

---
+++ src/Shark.Fido2.Core/Constants/AuthenticatorAttachment.cs
@@ -0,0 +1,9 @@
+﻿namespace Shark.Fido2.Core.Constants
+{
+    public static class AuthenticatorAttachment
+    {
+        public const string Platform = "platform";
+
+        public const string CrossPlatform = "cross-platform";
+    }
+}
