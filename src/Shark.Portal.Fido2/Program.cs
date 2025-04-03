beba8a19a49fa11433fb6635feed96f6dcf9d3d2
linuxchata <linuxchata@gmail.com>
2025-04-03 20:51:08 +0200

    Add dependencies

--- src/Shark.Portal.Fido2/Program.cs
+++ src/Shark.Portal.Fido2/Program.cs
@@ -1,6 +1,12 @@
+using Shark.Fido2.Core;
+using Shark.Fido2.InMemory;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+
+builder.Services.RegisterInMemoryRepositories();
+builder.Services.AddFido2(builder.Configuration);
 
 var app = builder.Build();
 
