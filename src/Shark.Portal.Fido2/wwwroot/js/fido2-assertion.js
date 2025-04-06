2ec514a635df4be55e6c3cecaf975b9e3196c6fb
linuxchata <linuxchata@gmail.com>
2025-04-06 10:16:12 +0200

    Add controllers and JS files

---
+++ src/Shark.Portal.Fido2/wwwroot/js/fido2-assertion.js
@@ -0,0 +1,91 @@
+﻿// Authentication
+
+const toastrAuthenticationTitle = 'Web Authentication';
+
+async function requestVerifyCredentialOptions(username) {
+    const optionsRequest = {
+        username: username
+    };
+
+    const options = await fetchAssertionOptions(optionsRequest);
+
+    await requestCredential(options);
+}
+
+async function requestCredential(options) {
+    const credentialRequestOptions = {
+        publicKey: {
+            challenge: toUint8Array(options.challenge),
+            timeout: options.timeout,
+            rpId: options.rpId
+        },
+    };
+
+    let assertion;
+    try {
+        assertion = await navigator.credentials.get(credentialRequestOptions);
+    }
+    catch (error) {
+        toastr.error(error.message, toastrAuthenticationTitle);
+        return;
+    }
+
+    const credentials = {
+        id: assertion.id,
+        rawId: toBase64Url(assertion.rawId),
+        response: {
+            authenticatorData: toBase64Url(assertion.response.authenticatorData),
+            clientDataJson: toBase64Url(assertion.response.clientDataJSON),
+            signature: toBase64Url(assertion.response.signature),
+            userHandle: toBase64Url(assertion.response.userHandle),
+        },
+        type: assertion.type,
+    };
+
+    await fetchAssertionResult(credentials);
+}
+
+async function fetchAssertionOptions(optionsRequest) {
+    try {
+        const response = await fetch('/assertion/options/', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(optionsRequest)
+        });
+
+        if (response.ok) {
+            return await response.json();
+        }
+        else {
+            toastr.error("Error creating authentication options", toastrAuthenticationTitle);
+        }
+    } catch (error) {
+        toastr.error(error.message, toastrAuthenticationTitle);
+    }
+}
+
+async function fetchAssertionResult(credentials) {
+    try {
+        const response = await fetch('/assertion/result/', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        });
+
+        if (response.ok) {
+            toastr.info('Authentication was successful', toastrAuthenticationTitle);
+        }
+        else {
+            const responseBody = await response.json();
+            toastr.error(`Authentication has failed. ${responseBody.errorMessage}`, toastrAuthenticationTitle);
+        }
+    } catch (error) {
+        toastr.error(error.message, toastrAuthenticationTitle);
+    }
+}
+
+window.requestVerifyCredentialOptions = requestVerifyCredentialOptions;
---
+++ src/Shark.Portal.Fido2/wwwroot/js/fido2-attestation.js
@@ -0,0 +1,109 @@
+﻿// Registration
+
+const toastrRegistrationTitle = 'Web Authentication';
+
+async function requestCreateCredentialOptions(username) {
+    const optionsRequest = {
+        username: username,
+        displayName: 'Shark',
+        attestation: 'direct'
+    };
+
+    const options = await fetchAttestationOptions(optionsRequest);
+
+    await createCredential(options);
+}
+
+async function createCredential(options) {
+    const extensions = options.extensions.credProps ? { 'credProps': options.extensions.credProps } : {};
+
+    const credentialCreationOptions = {
+        publicKey: {
+            rp: {
+                id: options.rp.id,
+                name: options.rp.name,
+            },
+            user: {
+                id: toUint8Array(options.user.id),
+                name: options.user.name,
+                displayName: options.user.displayName,
+            },
+            pubKeyCredParams: options.pubKeyCredParams.map(param => ({
+                type: param.type,
+                alg: param.alg,
+            })),
+            challenge: toUint8Array(options.challenge),
+            timeout: options.timeout,
+            attestation: options.attestation,
+            extensions: extensions
+        },
+    };
+
+    let attestation;
+    try {
+        attestation = await navigator.credentials.create(credentialCreationOptions);
+    }
+    catch (error) {
+        toastr.error(error.message, toastrRegistrationTitle);
+        return;
+    }
+
+    const credentials = {
+        id: attestation.id,
+        rawId: toBase64Url(attestation.rawId),
+        response: {
+            attestationObject: toBase64Url(attestation.response.attestationObject),
+            clientDataJson: toBase64Url(attestation.response.clientDataJSON),
+            transports: attestation.response.getTransports(),
+        },
+        type: attestation.type,
+        extensions: attestation.getClientExtensionResults(),
+    };
+
+    await fetchAttestationResult(credentials);
+}
+
+async function fetchAttestationOptions(optionsRequest) {
+    try {
+        const response = await fetch('/attestation/options/', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(optionsRequest)
+        });
+
+        if (response.ok) {
+            return await response.json();
+        }
+        else {
+            toastr.error("Error creating registration options", toastrRegistrationTitle);
+        }
+    } catch (error) {
+        toastr.error(error.message, toastrRegistrationTitle);
+    }
+}
+
+async function fetchAttestationResult(credentials) {
+    try {
+        const response = await fetch('/attestation/result/', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        });
+
+        if (response.ok) {
+            toastr.info('Registration was successful', toastrRegistrationTitle);
+        }
+        else {
+            const responseBody = await response.json();
+            toastr.error(`Registration has failed. ${responseBody.errorMessage}`, toastrRegistrationTitle);
+        }
+    } catch (error) {
+        toastr.error(error.message, toastrRegistrationTitle);
+    }
+}
+
+window.requestCreateCredentialOptions = requestCreateCredentialOptions;
