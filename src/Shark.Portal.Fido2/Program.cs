e8982d316af7427b414ec5da4e144b75244722d7
LinuxChata <linuxchata@gmail.com>
2025-04-24 20:54:46 +0200

    Add disclaimer about memory cache for the portal (#134)
    Add disclaimer about memory cache for the portal

--- src/Shark.Portal.Fido2/Program.cs
+++ src/Shark.Portal.Fido2/Program.cs
@@ -26,18 +26,18 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     app.UseHsts();
+
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Append("X-Frame-Options", "DENY");
+        context.Response.Headers.Append("Content-Security-Policy", "default-src 'self';");
+        context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+        context.Response.Headers.Append("Permissions-Policy", "geolocation=(), microphone=(), camera=()");
+        context.Response.Headers.Append("Referrer-Policy", "no-referrer");
+
+        await next();
+    });
 }
-
-app.Use(async (context, next) =>
-{
-    context.Response.Headers.Append("X-Frame-Options", "DENY");
-    context.Response.Headers.Append("Content-Security-Policy", "default-src 'self';");
-    context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
-    context.Response.Headers.Append("Permissions-Policy", "geolocation=(), microphone=(), camera=()");
-    context.Response.Headers.Append("Referrer-Policy", "no-referrer");
-
-    await next();
-});
 
 app.UseHttpsRedirection();
 app.UseWebOptimizer();
