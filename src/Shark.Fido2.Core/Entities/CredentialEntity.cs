49405813957b7b152da0146ebbe1c4e32c81aec5
LinuxChata <linuxchata@gmail.com>
2025-04-27 20:16:44 +0200

    Add entities for datastores (#141)
    Add entities for datastores

---
+++ src/Shark.Fido2.Core/Entities/CredentialEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace Shark.Fido2.Core.Entities;
+
+public sealed class CredentialEntity
+{
+    public required byte[] CredentialId { get; set; }
+
+    public required byte[] UserHandle { get; set; }
+
+    public required string Username { get; set; }
+
+    public required CredentialPublicKeyEntity CredentialPublicKey { get; set; }
+
+    public uint SignCount { get; set; }
+
+    public string[]? Transports { get; set; }
+}
---
+++ src/Shark.Fido2.Core/Entities/CredentialPublicKeyEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace Shark.Fido2.Core.Entities;
+
+public sealed class CredentialPublicKeyEntity
+{
+    public int KeyType { get; set; }
+
+    public int Algorithm { get; set; }
+
+    public byte[]? Modulus { get; set; }
+
+    public byte[]? Exponent { get; set; }
+
+    public int? Curve { get; set; }
+
+    public byte[]? XCoordinate { get; set; }
+
+    public byte[]? YCoordinate { get; set; }
+
+    public byte[]? Key { get; set; }
+}
