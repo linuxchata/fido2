4bd0781b981ef5cfe2ce67a1e02fcc470fc600bb
linuxchata <linuxchata@gmail.com>
2025-05-26 20:52:50 +0200

    Update JS script to support About page

--- src/Shark.Portal.Fido2/wwwroot/js/site.js
+++ src/Shark.Portal.Fido2/wwwroot/js/site.js
@@ -10,31 +10,37 @@
     }
 
     // Sign up button event listener
-    document.getElementById('signupButton').addEventListener('click', async function () {
-        const originalText = this.textContent; // this is the button
-        const username = document.getElementById('signupUsername').value;
-        const displayName = document.getElementById('displayName').value;
+    var signupButton = document.getElementById('signupButton');
+    if (signupButton && signupButton.addEventListener) {
+        signupButton.addEventListener('click', async function () {
+            const originalText = this.textContent; // this is the button
+            const username = document.getElementById('signupUsername').value;
+            const displayName = document.getElementById('displayName').value;
 
-        if (username && displayName) {
-            await handleAsyncAction(
-                this,
-                () => requestCreateCredentialOptions(username, displayName),
-                originalText
-            );
-        } else {
-            notify.error('Please fill in all required fields', 'Validation Error');
-        }
-    });
+            if (username && displayName) {
+                await handleAsyncAction(
+                    this,
+                    () => requestCreateCredentialOptions(username, displayName),
+                    originalText
+                );
+            } else {
+                notify.error('Please fill in all required fields', 'Validation Error');
+            }
+        });
+    }
 
     // Sign in button event listener
-    document.getElementById('signinButton').addEventListener('click', async function () {
-        const originalText = this.textContent; // this is the button
-        const username = document.getElementById('signinUsername').value;
+    var signinButton = document.getElementById('signinButton');
+    if (signinButton && signinButton.addEventListener) {
+        signinButton.addEventListener('click', async function () {
+            const originalText = this.textContent; // this is the button
+            const username = document.getElementById('signinUsername').value;
 
-        await handleAsyncAction(
-            this,
-            () => requestVerifyCredentialOptions(username),
-            originalText
-        );
-    });
+            await handleAsyncAction(
+                this,
+                () => requestVerifyCredentialOptions(username),
+                originalText
+            );
+        });
+    }
 });
