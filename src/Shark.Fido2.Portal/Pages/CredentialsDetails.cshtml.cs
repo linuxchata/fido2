48868249532f454ad413174a77cf403e5b4ff7af
linuxchata <linuxchata@gmail.com>
2025-05-31 19:50:13 +0200

    Improve pages

--- src/Shark.Fido2.Portal/Pages/CredentialsDetails.cshtml.cs
+++ src/Shark.Fido2.Portal/Pages/CredentialsDetails.cshtml.cs
@@ -1,10 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shark.Fido2.Common.Extensions;
+using Shark.Fido2.Portal.Services;
 
 namespace Shark.Fido2.Portal.Pages;
 
 public class CredentialsDetailsModel : PageModel
 {
+    private readonly ICredentialService _credentialService;
+
+    public CredentialsDetailsModel(ICredentialService credentialService)
+    {
+        _credentialService = credentialService;
+    }
+
     [BindProperty]
     public required byte[] CredentialId { get; set; }
 
@@ -20,7 +29,24 @@ public class CredentialsDetailsModel : PageModel
     [BindProperty]
     public string[]? Transports { get; set; }
 
-    public void OnGet()
+    public async Task OnGet(string credentialId, CancellationToken cancellationToken)
     {
+        if (string.IsNullOrEmpty(credentialId))
+        {
+            return;
+        }
+
+        var credential = await _credentialService.Get(credentialId.FromBase64Url(), cancellationToken);
+        if (credential is null)
+        {
+            Response.Redirect("/");
+            return;
+        }
+
+        CredentialId = credential!.CredentialId;
+        UserHandle = credential.UserHandle;
+        Username = credential.Username;
+        SignCount = credential.SignCount;
+        Transports = credential.Transports;
     }
-} 
+}
