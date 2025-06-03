31c4d60836feaf61251ff069e39a79daa0b72d9a
linuxchata <linuxchata@gmail.com>
2025-06-03 21:52:13 +0200

    Swap the dictionary

--- src/Shark.Fido2.Portal/PublicKeyAlgorithms.cs
+++ src/Shark.Fido2.Portal/PublicKeyAlgorithms.cs
@@ -2,24 +2,24 @@
 
 public static class PublicKeyAlgorithms
 {
-    private readonly static Dictionary<string, int> _algorithms = new Dictionary<string, int>
+    private readonly static Dictionary<int, string> _algorithms = new()
     {
-        { "ES256 (ECDSA w/ SHA-256)", -7 },
-        { "EdDSA (EdDSA)", -8 },
-        { "ES384 (ECDSA w/ SHA-384)", -35 },
-        { "ES512 (ECDSA w/ SHA-512)", -36 },
-        { "PS256 (RSASSA-PSS w/ SHA-256)", -37 },
-        { "PS384 (RSASSA-PSS w/ SHA-384)", -38 },
-        { "PS512 (RSASSA-PSS w/ SHA-512)", -39 },
-        { "ES256K (ECDSA using secp256k1 curve and SHA-256)", -47 },
-        { "RS256 (RSASSA-PKCS1-v1_5 using SHA-256)", -257 },
-        { "RS384 (RSASSA-PKCS1-v1_5 using SHA-384)", -258 },
-        { "RS512 (RSASSA-PKCS1-v1_5 using SHA-512)", -259 },
-        { "RS1 (RSASSA-PKCS1-v1_5 using SHA-1)", -65535 }
+        { -7, "ES256 (ECDSA w/ SHA-256)" },
+        { -8, "EdDSA (EdDSA)" },
+        { -35, "ES384 (ECDSA w/ SHA-384)" },
+        { -36, "ES512 (ECDSA w/ SHA-512)" },
+        { -37, "PS256 (RSASSA-PSS w/ SHA-256)" },
+        { -38, "PS384 (RSASSA-PSS w/ SHA-384)" },
+        { -39, "PS512 (RSASSA-PSS w/ SHA-512)" },
+        { -47, "ES256K (ECDSA using secp256k1 curve and SHA-256)" },
+        { -257, "RS256 (RSASSA-PKCS1-v1_5 using SHA-256)" },
+        { -258, "RS384 (RSASSA-PKCS1-v1_5 using SHA-384)" },
+        { -259, "RS512 (RSASSA-PKCS1-v1_5 using SHA-512)" },
+        { -65535, "RS1 (RSASSA-PKCS1-v1_5 using SHA-1)" }
     };
 
-    public static string Get(int value)
+    public static string Get(int key)
     {
-        return _algorithms.FirstOrDefault(x => x.Value == value).Key ?? "Unknown Algorithm";
+        return _algorithms.FirstOrDefault(x => x.Key == key).Value ?? "Unknown";
     }
 }
