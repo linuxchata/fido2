83a8bcca278001556eeff877d31d35a2e3072745
linuxchata <linuxchata@gmail.com>
2025-06-08 17:04:22 +0200

    Update XML comments

--- src/Shark.Fido2.Core/Configurations/Fido2Configuration.cs
+++ src/Shark.Fido2.Core/Configurations/Fido2Configuration.cs
@@ -5,32 +5,40 @@ public sealed class Fido2Configuration
     public const string Name = nameof(Fido2Configuration);
 
     /// <summary>
-    /// Gets the Relying Party origin.
-    /// </summary>
-    public required string Origin { get; init; }
-
-    /// <summary>
-    /// Gets a valid domain string identifying the Relying Party.
+    /// Gets a valid domain string identifying the Relying Party on whose behalf a given registration or
+    /// authentication ceremony is being performed. This is a critical parameter in the WebAuthn protocol.
+    /// It defines the security scope within which credentials are valid. Therefore, careful selection is
+    /// essential, as an incorrect or overly broad value can lead to unintended credential reuse or security
+    /// vulnerabilities.
     /// </summary>
     public required string RelyingPartyId { get; init; }
 
     /// <summary>
-    /// Gets a name of the Relying Party.
+    /// Gets a human-palatable identifier for the Relying Party, intended only for display.
     /// </summary>
     public required string RelyingPartyIdName { get; init; }
 
     /// <summary>
-    /// Gets a time, in milliseconds, that the caller is willing to wait for the call to complete.
+    /// Gets a fully qualified origin of the Relying Party making the request, passed to the authenticator
+    /// by the browser.
+    /// </summary>
+    public required string Origin { get; init; }
+
+    /// <summary>
+    /// Gets a time, in milliseconds, that the Relying Party is willing to wait for the call to complete.
     /// </summary>
     public ulong? Timeout { get; init; }
 
     /// <summary>
     /// Gets or sets a value indicating whether None attestation type is acceptable under Relying Party policy.
+    /// None attestation is used when the authenticator doesn't have any attestation information available.
     /// </summary>
     public bool AllowNoneAttestation { get; set; } = true;
 
     /// <summary>
     /// Gets or sets a value indicating whether Self attestation type is acceptable under Relying Party policy.
+    /// Self attestation is used when the authenticator doesn't have a dedicated attestation key pair or
+    /// a vendor-issued certificate.
     /// </summary>
     public bool AllowSelfAttestation { get; set; } = true;
 
@@ -41,12 +49,14 @@ public sealed class Fido2Configuration
     public bool EnableTrustedExecutionEnvironmentOnly { get; set; } = true;
 
     /// <summary>
-    /// Gets or sets a value indicating whether the Relying Party uses the Metadata Service to verify the attestation object.
+    /// Gets or sets a value indicating whether the Relying Party uses the Metadata Service to verify
+    /// the attestation object.
     /// </summary>
     public bool EnableMetadataService { get; set; } = true;
 
     /// <summary>
     /// Gets or sets a value indicating whether the Relying Party requires strict verification of authenticators.
+    /// If enabled, missing metadata for the authenticator would cause attestation to fail.
     /// </summary>
     public bool EnableStrictAuthenticatorVerification { get; set; } = false;
 
