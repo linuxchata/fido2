a51660e24abe445c9e6fde360ed2666c32df3db4
linuxchata <linuxchata@gmail.com>
2025-06-15 13:17:02 +0200

    Add AWS DynamoDB data storage

---
+++ src/Shark.Fido2.DynamoDB/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shark.Fido2.Core.Abstractions.Repositories;
+
+namespace Shark.Fido2.DynamoDB;
+
+public static class DependencyInjection
+{
+    public static void AddFido2DynamoDB(this IServiceCollection services)
+    {
+        if (services.Any(s => s.ServiceType == typeof(ICredentialRepository)))
+        {
+            throw new InvalidOperationException("Credential repository can only be registered once.");
+        }
+    }
+}
