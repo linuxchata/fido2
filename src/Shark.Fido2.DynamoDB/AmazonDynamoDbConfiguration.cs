23fb18dc0f89d6048f236dda307694a581617bab
linuxchata <linuxchata@gmail.com>
2025-06-15 16:49:24 +0200

    Add read operation

---
+++ src/Shark.Fido2.DynamoDB/AmazonDynamoDbConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Shark.Fido2.DynamoDB;
+
+internal class AmazonDynamoDbConfiguration
+{
+    public string AwsRegion { get; set; } = null!;
+
+    public string AccessKey { get; set; } = null!;
+
+    public string SecretKey { get; set; } = null!;
+}
