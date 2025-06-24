4332efadc70e3856f6031658e8d21cbc963a11f1
linuxchata <linuxchata@hotmail.com>
2025-06-25 00:29:27 +0200

    Add call to clean the creds button

--- src/Shark.Fido2.Portal/wwwroot/js/site.js
+++ src/Shark.Fido2.Portal/wwwroot/js/site.js
@@ -38,6 +38,8 @@
                 return;
             }
 
+            toggleCredentialsButtonVisibility();
+
             await handleAsyncAction(
                 this,
                 () => requestCreateCredentialOptions(userName, displayName),
