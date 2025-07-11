c61a0f5832645585128f393043fa4f84aa252608
LinuxChata <linuxchata@hotmail.com>
2025-07-11 22:42:44 +0200

    Small improvements for Portal's UI / UX (#243)
    Small improvements for Portal's UI / UX

--- src/Shark.Fido2.Portal/wwwroot/js/site.js
+++ src/Shark.Fido2.Portal/wwwroot/js/site.js
@@ -13,11 +13,11 @@
     }
 
     const credentialIdInput = getById('credentialId');
-    const credentialsButton = getById('credentialsButton');
+    const credentialsLink = getById('credentialsLink');
 
     function toggleCredentialsButtonVisibility() {
-        if (credentialsButton && credentialIdInput) {
-            credentialsButton.classList.toggle('hide', !credentialIdInput.value);
+        if (credentialsLink && credentialIdInput) {
+            credentialsLink.classList.toggle('hide', !credentialIdInput.value);
         }
     }
 
@@ -65,13 +65,13 @@
         });
     }
 
-    if (credentialsButton) {
-        credentialsButton.addEventListener('click', () => {
+    if (credentialsLink) {
+        credentialsLink.addEventListener('click', () => {
             if (credentialIdInput) {
                 const credentialId = credentialIdInput?.value;
 
                 if (credentialId) {
-                    window.location.href = `/CredentialsDetails?credentialId=${encodeURIComponent(credentialId)}`;
+                    window.location.href = `/CredentialDetails?credentialId=${encodeURIComponent(credentialId)}`;
                 }
             }
         });
