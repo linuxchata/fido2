5dd722fcb15e08142c7ad250e4673abda6007844
linuxchata <linuxchata@hotmail.com>
2025-07-18 22:00:27 +0200

    Relax Content-Security-Policy for images

--- src/Shark.Fido2.Portal/Program.cs
+++ src/Shark.Fido2.Portal/Program.cs
@@ -51,7 +51,7 @@ if (!app.Environment.IsDevelopment())
     app.Use(async (context, next) =>
     {
         context.Response.Headers.Append("X-Frame-Options", "DENY");
-        context.Response.Headers.Append("Content-Security-Policy", "default-src 'self'; img-src 'self' data:;");
+        context.Response.Headers.Append("Content-Security-Policy", "default-src 'self'; img-src https: data:;");
         context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
         context.Response.Headers.Append("Permissions-Policy", "geolocation=(), microphone=(), camera=()");
         context.Response.Headers.Append("Referrer-Policy", "no-referrer");
