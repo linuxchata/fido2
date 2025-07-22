20ae3fd7923ba47fe5d94ddc9fed3ff97cd1ec03
linuxchata <linuxchata@hotmail.com>
2025-07-22 08:21:23 +0200

    Improve styling of the Index page

--- src/Shark.Fido2.Sample/wwwroot/js/site.js
+++ src/Shark.Fido2.Sample/wwwroot/js/site.js
@@ -9,16 +9,20 @@
 };
 
 $(function () {
-    $(".btn-primary").on("click", function (event) {
-        let actionType = $(this).data("action");
-        let usernameInput = $("#username");
-        let displayNameInput = $("#displayName");
-        let messageSpan = $("#message");
-        let username = usernameInput.val();
-        let displayName = displayNameInput.val();
+    $("#register").on("click", function (event) {
+        const usernameInput = $("#username-register");
+        const displayNameInput = $("#display-name-register");
+        const messageSpan = $("#message-register");
+        const username = usernameInput.val();
+        const displayName = displayNameInput.val();
 
-        if (isRegister(actionType) && !isValidInput(username)) {
+        if (!isValidInput(username)) {
             messageSpan.text("Please input a username");
+            return;
+        }
+
+        if (!isValidInput(displayName)) {
+            messageSpan.text("Please input a display name");
             return;
         }
 
@@ -26,16 +30,38 @@ $(function () {
         displayNameInput.prop("readonly", true);
         messageSpan.text("");
 
-        let button = event.target;
-        let previousText = button.innerHTML;
+        const button = event.target;
+        const previousText = button.innerHTML;
         disableButton(button);
 
-        (isRegister(actionType) ?
-            requestCreateCredentialOptions(username, displayName) :
-            requestVerifyCredentialOptions(username))
+        requestCreateCredentialOptions(username, displayName)
             .finally(() => {
                 usernameInput.prop("readonly", false);
                 displayNameInput.prop("readonly", false);
+                enableButton(button, previousText);
+            });
+    });
+
+    $("#authenticate").on("click", function (event) {
+        const usernameInput = $("#username-authenticate");
+        const messageSpan = $("#message-authenticate");
+        const username = usernameInput.val();
+
+        if (!isValidInput(username)) {
+            messageSpan.text("Please input a username");
+            return;
+        }
+
+        usernameInput.prop("readonly", true);
+        messageSpan.text("");
+
+        const button = event.target;
+        const previousText = button.innerHTML;
+        disableButton(button);
+
+        requestVerifyCredentialOptions(username)
+            .finally(() => {
+                usernameInput.prop("readonly", false);
                 enableButton(button, previousText);
             });
     });
