6613b81006cce21924c37cec60ca36d837d09034
linuxchata <linuxchata@hotmail.com>
2025-07-24 20:20:43 +0200

    Fix small issue

--- src/Shark.Fido2.Metadata.Core/Repositories/HttpClientRepository.cs
+++ src/Shark.Fido2.Metadata.Core/Repositories/HttpClientRepository.cs
@@ -19,7 +19,7 @@ internal sealed class HttpClientRepository : IHttpClientRepository
         using var client = new HttpClient();
         using var stream = await client.GetStreamAsync(_configuration.MetadataBlobLocation, cancellationToken);
         using var reader = new StreamReader(stream);
-        return await reader.ReadToEndAsync();
+        return await reader.ReadToEndAsync(cancellationToken);
     }
 
     public async Task<X509Certificate2> GetRootCertificate(CancellationToken cancellationToken)
