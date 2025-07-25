744915822705e7958a9e6c0a467a05a6f634d43c
LinuxChata <linuxchata@hotmail.com>
2025-07-25 07:14:40 +0200

    Remove portal (#299)
    Remove portal. Portal is now location in https://github.com/linuxchata/fido2-portal

--- src/Shark.Fido2.Portal/Services/ICredentialService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Shark.Fido2.Domain;
-
-namespace Shark.Fido2.Portal.Services;
-
-public interface ICredentialService
-{
-    Task<Credential?> Get(byte[] id, CancellationToken cancellationToken);
-}
