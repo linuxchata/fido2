5f0624b624800877380e4025d23150ddf1bec31e
LinuxChata <linuxchata@hotmail.com>
2025-07-26 12:15:38 +0200

    Refactor client data validator (#304)
    Refactor client data validator

--- tests/Shark.Fido2.Core.Tests/Validators/ClientDataValidatorTests.cs
+++ tests/Shark.Fido2.Core.Tests/Validators/ClientDataValidatorTests.cs
@@ -17,7 +17,7 @@ internal class ClientDataValidatorTests
     }
 
     [Test]
-    public void Validate_WhenClientDataValid_ThenReturnsValidResult()
+    public void ValidateForAttestation_WhenClientDataValid_ThenReturnsValidResult()
     {
         // Arrange
         var expectedChallenge = "t2pJGIQ7Y4DXF2b98tnBjg";
@@ -41,7 +41,7 @@ internal class ClientDataValidatorTests
     }
 
     [Test]
-    public void Validate_WhenClientDataValidWithTokenBinding_ThenReturnsValidResult()
+    public void ValidateForAttestation_WhenClientDataValidWithTokenBinding_ThenReturnsValidResult()
     {
         // Arrange
         var expectedChallenge = "t2pJGIQ7Y4DXF2b98tnBjg";
@@ -70,7 +70,67 @@ internal class ClientDataValidatorTests
     }
 
     [Test]
-    public void Validate_WhenClientDataInvalidWithMissmatchedChallenge_ThenReturnsInvalidResult()
+    public void ValidateForAttestation_WhenTypeIsInvalid_ThenReturnsInvalidResult()
+    {
+        var expectedChallenge = "expected-challenge";
+
+        var clientDataModel = new ClientData
+        {
+            Type = "invalid",
+            Challenge = expectedChallenge,
+            Origin = "https://localhost:4000",
+            CrossOrigin = false,
+            ClientDataHash = [],
+        };
+
+        var result = _sut.ValidateForAttestation(clientDataModel, expectedChallenge);
+
+        Assert.That(result.IsValid, Is.False);
+        Assert.That(result.Message, Does.Contain("Client data type mismatch"));
+    }
+
+    [Test]
+    public void ValidateForAttestation_WhenOriginIsMalformed_ThenReturnsInvalidResult()
+    {
+        var expectedChallenge = "t2pJGIQ7Y4DXF2b98tnBjg";
+
+        var clientDataModel = new ClientData
+        {
+            Type = WebAuthnType.Create,
+            Challenge = expectedChallenge,
+            Origin = "invalid-uri",
+            CrossOrigin = false,
+            ClientDataHash = [],
+        };
+
+        var result = _sut.ValidateForAttestation(clientDataModel, expectedChallenge);
+
+        Assert.That(result.IsValid, Is.False);
+        Assert.That(result.Message, Does.Contain("Invalid client data origin"));
+    }
+
+    [Test]
+    public void ValidateForAttestation_WhenOriginIsNotAllowed_ThenReturnsInvalidResult()
+    {
+        var expectedChallenge = "t2pJGIQ7Y4DXF2b98tnBjg";
+
+        var clientDataModel = new ClientData
+        {
+            Type = WebAuthnType.Create,
+            Challenge = expectedChallenge,
+            Origin = "https://not-allowed.com",
+            CrossOrigin = false,
+            ClientDataHash = [],
+        };
+
+        var result = _sut.ValidateForAttestation(clientDataModel, expectedChallenge);
+
+        Assert.That(result.IsValid, Is.False);
+        Assert.That(result.Message, Does.Contain("Client data origin mismatch"));
+    }
+
+    [Test]
+    public void ValidateForAttestation_WhenMissmatchedChallenge_ThenReturnsInvalidResult()
     {
         // Arrange
         var clientDataModel = new ClientData
@@ -92,7 +152,7 @@ internal class ClientDataValidatorTests
     }
 
     [Test]
-    public void Validate_WhenClientDataValidWithMissingTokenBindingId_ThenReturnsInvalidResult()
+    public void ValidateForAttestation_WhenMissingTokenBindingId_ThenReturnsInvalidResult()
     {
         // Arrange
         var expectedChallenge = "t2pJGIQ7Y4DXF2b98tnBjg";
@@ -112,6 +172,169 @@ internal class ClientDataValidatorTests
 
         // Act
         var result = _sut.ValidateForAttestation(clientDataModel, $"{expectedChallenge}==");
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.IsValid, Is.False);
+        Assert.That(result.Message, Is.Not.Null);
+    }
+
+    [Test]
+    public void ValidateForAssertion_WhenClientDataValid_ThenReturnsValidResult()
+    {
+        // Arrange
+        var expectedChallenge = "t2pJGIQ7Y4DXF2b98tnBjg";
+
+        var clientDataModel = new ClientData
+        {
+            Type = WebAuthnType.Get,
+            Challenge = expectedChallenge,
+            Origin = "https://localhost:4000",
+            CrossOrigin = false,
+            ClientDataHash = [],
+        };
+
+        // Act
+        var result = _sut.ValidateForAssertion(clientDataModel, expectedChallenge);
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.IsValid, Is.True);
+        Assert.That(result.Message, Is.Null);
+    }
+
+    [Test]
+    public void ValidateForAssertion_WhenClientDataValidWithTokenBinding_ThenReturnsValidResult()
+    {
+        // Arrange
+        var expectedChallenge = "t2pJGIQ7Y4DXF2b98tnBjg";
+
+        var clientDataModel = new ClientData
+        {
+            Type = WebAuthnType.Get,
+            Challenge = expectedChallenge,
+            Origin = "https://localhost:4000",
+            CrossOrigin = false,
+            ClientDataHash = [],
+            TokenBinding = new TokenBinding
+            {
+                Id = "7864716657891",
+                Status = Domain.Enums.TokenBindingStatus.Present,
+            },
+        };
+
+        // Act
+        var result = _sut.ValidateForAssertion(clientDataModel, expectedChallenge);
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.IsValid, Is.True);
+        Assert.That(result.Message, Is.Null);
+    }
+
+    [Test]
+    public void ValidateForAssertion_WhenTypeIsInvalid_ThenReturnsInvalidResult()
+    {
+        var expectedChallenge = "expected-challenge";
+
+        var clientDataModel = new ClientData
+        {
+            Type = "invalid",
+            Challenge = expectedChallenge,
+            Origin = "https://localhost:4000",
+            CrossOrigin = false,
+            ClientDataHash = [],
+        };
+
+        var result = _sut.ValidateForAssertion(clientDataModel, expectedChallenge);
+
+        Assert.That(result.IsValid, Is.False);
+        Assert.That(result.Message, Does.Contain("Client data type mismatch"));
+    }
+
+    [Test]
+    public void ValidateForAssertion_WhenOriginIsMalformed_ThenReturnsInvalidResult()
+    {
+        var expectedChallenge = "t2pJGIQ7Y4DXF2b98tnBjg";
+
+        var clientDataModel = new ClientData
+        {
+            Type = WebAuthnType.Get,
+            Challenge = expectedChallenge,
+            Origin = "invalid-uri",
+            CrossOrigin = false,
+            ClientDataHash = [],
+        };
+
+        var result = _sut.ValidateForAssertion(clientDataModel, expectedChallenge);
+
+        Assert.That(result.IsValid, Is.False);
+        Assert.That(result.Message, Does.Contain("Invalid client data origin"));
+    }
+
+    [Test]
+    public void ValidateForAssertion_WhenOriginIsNotAllowed_ThenReturnsInvalidResult()
+    {
+        var expectedChallenge = "t2pJGIQ7Y4DXF2b98tnBjg";
+
+        var clientDataModel = new ClientData
+        {
+            Type = WebAuthnType.Get,
+            Challenge = expectedChallenge,
+            Origin = "https://not-allowed.com",
+            CrossOrigin = false,
+            ClientDataHash = [],
+        };
+
+        var result = _sut.ValidateForAssertion(clientDataModel, expectedChallenge);
+
+        Assert.That(result.IsValid, Is.False);
+        Assert.That(result.Message, Does.Contain("Client data origin mismatch"));
+    }
+
+    [Test]
+    public void ValidateForAssertion_WhenMissmatchedChallenge_ThenReturnsInvalidResult()
+    {
+        // Arrange
+        var clientDataModel = new ClientData
+        {
+            Type = WebAuthnType.Get,
+            Challenge = "t2pJGIQ7Y4DXF2b98tnBjg",
+            Origin = "https://localhost:4000",
+            CrossOrigin = false,
+            ClientDataHash = [],
+        };
+
+        // Act
+        var result = _sut.ValidateForAssertion(clientDataModel, "1epJGYQ2Y9DXF1b98tnGwr==");
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.IsValid, Is.False);
+        Assert.That(result.Message, Is.Not.Null);
+    }
+
+    [Test]
+    public void ValidateForAssertion_WhenMissingTokenBindingId_ThenReturnsInvalidResult()
+    {
+        // Arrange
+        var expectedChallenge = "t2pJGIQ7Y4DXF2b98tnBjg";
+
+        var clientDataModel = new ClientData
+        {
+            Type = WebAuthnType.Get,
+            Challenge = expectedChallenge,
+            Origin = "https://localhost:4000",
+            CrossOrigin = false,
+            ClientDataHash = [],
+            TokenBinding = new TokenBinding
+            {
+                Status = Domain.Enums.TokenBindingStatus.Present,
+            },
+        };
+
+        // Act
+        var result = _sut.ValidateForAssertion(clientDataModel, $"{expectedChallenge}==");
 
         // Assert
         Assert.That(result, Is.Not.Null);
