805aed184fbd59eee76f4e3994e433e9291d4dc4
linuxchata <linuxchata@hotmail.com>
2025-07-27 17:51:51 +0200

    Improve Content-Security-Policy header

--- src/Shark.Fido2.Sample/Program.cs
+++ src/Shark.Fido2.Sample/Program.cs
@@ -46,7 +46,7 @@ if (!app.Environment.IsDevelopment())
     app.Use(async (context, next) =>
     {
         context.Response.Headers.Append("X-Frame-Options", "DENY");
-        context.Response.Headers.Append("Content-Security-Policy", "default-src 'self'; img-src 'self' data:;");
+        context.Response.Headers.Append("Content-Security-Policy", "default-src 'self'; img-src 'self' data:; form-action 'self'; frame-ancestors 'none';");
         context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
         context.Response.Headers.Append("Permissions-Policy", "geolocation=(), microphone=(), camera=()");
         context.Response.Headers.Append("Referrer-Policy", "no-referrer");
