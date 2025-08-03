d411bf4ad4bdc9b38e73a9f4fec85ef77f73224f
LinuxChata <linuxchata@hotmail.com>
2025-08-03 15:56:13 +0200

    Improve logic of user id generator and add XML comments (#347)
    Improve logic of user id generator and add XML comments

--- src/Shark.Fido2.Core/Abstractions/IChallengeGenerator.cs
+++ src/Shark.Fido2.Core/Abstractions/IChallengeGenerator.cs
@@ -1,6 +1,13 @@
 ﻿namespace Shark.Fido2.Core.Abstractions;
 
+/// <summary>
+/// The interface representing the logic to generate cryptographic challenge values.
+/// </summary>
 public interface IChallengeGenerator
 {
+    /// <summary>
+    /// Gets a new cryptographic challenge value.
+    /// </summary>
+    /// <returns>A byte array containing the generated challenge.</returns>
     byte[] Get();
 }
