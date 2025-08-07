a465e0dad224af1fa14d21a0029ca2836582859a
linuxchata <linuxchata@hotmail.com>
2025-08-07 22:54:37 +0200

    Configure HSTS and disable optional HTTP methods

---
+++ src/Shark.Fido2.Sample/Middlewares/DisableTrackMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace Shark.Fido2.Sample.Middlewares;
+
+public class DisableTrackMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task Invoke(HttpContext context)
+    {
+        if (string.Equals(context.Request.Method, "TRACE", StringComparison.OrdinalIgnoreCase) ||
+            string.Equals(context.Request.Method, "TRACK", StringComparison.OrdinalIgnoreCase) ||
+            string.Equals(context.Request.Method, "OPTIONS", StringComparison.OrdinalIgnoreCase))
+        {
+            context.Response.StatusCode = StatusCodes.Status405MethodNotAllowed;
+            return;
+        }
+
+        await _next(context);
+    }
+}
