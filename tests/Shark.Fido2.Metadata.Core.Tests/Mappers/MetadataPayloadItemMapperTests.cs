04ef7852ed2d37191479c997ec90090cda24c7ba
linuxchata <linuxchata@hotmail.com>
2025-08-15 17:06:39 +0200

    Add unit tests for MetadataBlobPayloadEntry

--- tests/Shark.Fido2.Metadata.Core.Tests/Mappers/MetadataPayloadItemMapperTests.cs
+++ tests/Shark.Fido2.Metadata.Core.Tests/Mappers/MetadataPayloadItemMapperTests.cs
@@ -42,7 +42,7 @@ internal class MetadataPayloadItemMapperTests
             },
             StatusReports =
             [
-                new Models.StatusReport
+                new StatusReport
                 {
                     Status = "FIDO_CERTIFIED",
                     EffectiveDate = DateTime.UtcNow.ToString(),
