6aa38e48f6f8cfd142b60f7fcf8fa9814497588f
linuxchata <linuxchata@hotmail.com>
2025-08-21 21:55:52 +0200

    Add client side integration with WebAuthn API

---
+++ src/Shark.Fido2.Sample.Blazor/Shark.Fido2.Sample.Blazor/wwwroot/js/assertion.js
@@ -0,0 +1,124 @@
+﻿// Authentication with a public key credential using the Web Authentication API
+
+const authenticationTitle = 'Web Authentication';
+
+async function authentication(username, displayName) {
+    const optionsRequest = { username };
+
+    await authenticationCustom(optionsRequest);
+}
+
+async function authenticationCustom(optionsRequest) {
+    console.log("Start fetching assertion options");
+
+    const options = await fetchAssertionOptions(optionsRequest);
+
+    console.log(`Server side assertion options\n${JSON.stringify(options)}`);
+
+    await requestCredential(options);
+}
+
+async function requestCredential(options) {
+    let extensions = {
+        ...(options.extensions.appid && { appid: options.extensions.appid }),
+        ...(options.extensions.uvm && { uvm: options.extensions.uvm }),
+        ...(options.extensions.largeBlob && { largeBlob: options.extensions.largeBlob })
+    };
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/PublicKeyCredentialRequestOptions
+    const credentialRequestOptions = {
+        publicKey: {
+            rpId: options.rpId,
+            userVerification: options.userVerification,
+            challenge: toUint8Array(options.challenge),
+            allowCredentials: options.allowCredentials.map(credential => ({
+                id: toUint8Array(credential.id),
+                transports: credential.transports,
+                type: credential.type,
+            })),
+            timeout: options.timeout,
+            extensions: extensions
+        },
+    };
+
+    console.log(`Mapped assertion options\n${JSON.stringify(credentialRequestOptions)}`);
+
+    let assertion;
+    try {
+        assertion = await navigator.credentials.get(credentialRequestOptions);
+    }
+    catch (error) {
+        notify.error(error.message, authenticationTitle);
+        return;
+    }
+
+    console.log("Assertion object was received from browser");
+
+    const credentials = {
+        id: assertion.id,
+        rawId: toBase64Url(assertion.rawId),
+        response: {
+            authenticatorData: toBase64Url(assertion.response.authenticatorData),
+            clientDataJson: toBase64Url(assertion.response.clientDataJSON),
+            signature: toBase64Url(assertion.response.signature),
+            userHandle: toBase64Url(assertion.response.userHandle),
+        },
+        type: assertion.type,
+        extensions: assertion.getClientExtensionResults(),
+    };
+
+    console.log(`Mapped assertion object ${JSON.stringify(credentials)}`);
+
+    await fetchAssertionResult(credentials);
+
+    console.log("Assertion was completed on server side");
+}
+
+async function fetchAssertionOptions(optionsRequest) {
+    try {
+        const response = await fetch('/assertion/options/', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(optionsRequest)
+        });
+
+        if (response.ok) {
+            return await response.json();
+        }
+        else {
+            const errorMessage = await response.text();
+            throw new Error(`Server responded with status code ${response.status}: ${errorMessage}`);
+        }
+    } catch (error) {
+        notify.error("Error creating authentication options", authenticationTitle);
+        throw error;
+    }
+}
+
+async function fetchAssertionResult(credentials) {
+    try {
+        const response = await fetch('/assertion/result/', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        });
+
+        if (response.ok) {
+            notify.info('Authentication was successful', authenticationTitle);
+            window.location.href = `/details`;
+        }
+        else {
+            const responseBody = await response.json();
+            throw new Error(responseBody.errorMessage);
+        }
+    } catch (error) {
+        notify.error(`Authentication has failed. ${error.message}`, authenticationTitle);
+    }
+}
+
+window.authentication = authentication;
+window.authenticationCustom = authenticationCustom;
---
+++ src/Shark.Fido2.Sample.Blazor/Shark.Fido2.Sample.Blazor/wwwroot/js/attestation.js
@@ -0,0 +1,150 @@
+﻿// Registration of a public key credential using the Web Authentication API
+
+const registrationTitle = 'Web Authentication';
+
+async function registration(username, displayName) {
+    const optionsRequest = {
+        username: username,
+        displayName: displayName,
+        attestation: 'direct',
+        authenticatorSelection: {
+            residentKey: 'preferred',
+            userVerification: 'preferred',
+            requireResidentKey: false
+        }
+    };
+
+    await registrationCustom(optionsRequest);
+}
+
+async function registrationCustom(optionsRequest) {
+    console.log("Start fetching attestation options");
+
+    const options = await fetchAttestationOptions(optionsRequest);
+
+    console.log(`Server side attestation options\n${JSON.stringify(options)}`);
+
+    await createCredential(options);
+}
+
+async function createCredential(options) {
+    let extensions = {
+        ...(options.extensions.appidExclude && { appidExclude: options.extensions.appidExclude }),
+        ...(options.extensions.uvm && { uvm: options.extensions.uvm }),
+        ...(options.extensions.credProps && { credProps: options.extensions.credProps }),
+        ...(options.extensions.largeBlob && { largeBlob: options.extensions.largeBlob })
+    };
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/PublicKeyCredentialCreationOptions
+    const credentialCreationOptions = {
+        publicKey: {
+            rp: {
+                id: options.rp.id,
+                name: options.rp.name,
+            },
+            user: {
+                id: toUint8Array(options.user.id),
+                name: options.user.name,
+                displayName: options.user.displayName,
+            },
+            pubKeyCredParams: options.pubKeyCredParams.map(param => ({
+                type: param.type,
+                alg: param.alg,
+            })),
+            authenticatorSelection: options.authenticatorSelection,
+            challenge: toUint8Array(options.challenge),
+            excludeCredentials: options.excludeCredentials.map(credential => ({
+                id: toUint8Array(credential.id),
+                transports: credential.transports,
+                type: credential.type,
+            })),
+            timeout: options.timeout,
+            attestation: options.attestation,
+            extensions: extensions
+        },
+    };
+
+    console.log(`Mapped attestation options\n${JSON.stringify(credentialCreationOptions)}`);
+
+    let attestation;
+    try {
+        attestation = await navigator.credentials.create(credentialCreationOptions);
+    }
+    catch (error) {
+        if (error.name === 'InvalidStateError') {
+            notify.error('The authenticator was not allowed because it was already registered.', registrationTitle);
+        }
+        else {
+            notify.error(error.message, registrationTitle);
+        }
+        return;
+    }
+
+    console.log("Attestation object was received from browser");
+
+    const credentials = {
+        id: attestation.id,
+        rawId: toBase64Url(attestation.rawId),
+        response: {
+            attestationObject: toBase64Url(attestation.response.attestationObject),
+            clientDataJson: toBase64Url(attestation.response.clientDataJSON),
+            transports: attestation.response.getTransports(),
+        },
+        type: attestation.type,
+        extensions: attestation.getClientExtensionResults(),
+    };
+
+    console.log(`Mapped attestation object ${JSON.stringify(credentials)}`);
+
+    await fetchAttestationResult(credentials);
+
+    console.log("Attestation was completed on server side");
+}
+
+async function fetchAttestationOptions(optionsRequest) {
+    try {
+        const response = await fetch('/attestation/options/', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(optionsRequest)
+        });
+
+        if (response.ok) {
+            return await response.json();
+        }
+        else {
+            const errorMessage = await response.text();
+            throw new Error(`Server responded with status code ${response.status}: ${errorMessage}`);
+        }
+    } catch (error) {
+        notify.error("Error creating registration options", registrationTitle);
+        throw error;
+    }
+}
+
+async function fetchAttestationResult(credentials) {
+    try {
+        const response = await fetch('/attestation/result/', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        });
+
+        if (response.ok) {
+            notify.info('Registration was successful', registrationTitle);
+        }
+        else {
+            const responseBody = await response.json();
+            throw new Error(responseBody.errorMessage);
+        }
+    } catch (error) {
+        notify.error(`Registration has failed. ${error.message}`, registrationTitle);
+    }
+}
+
+window.registration = registration;
+window.registrationCustom = registrationCustom;
