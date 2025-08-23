db7a3babbfaaefc7c1cc9a9c95873ff512058f8c
linuxchata <linuxchata@hotmail.com>
2025-08-23 11:50:52 +0200

    Add credential service

---
+++ src/Shark.Fido2.Sample.Blazor/Shark.Fido2.Sample.Blazor/Abstractions/Services/ICredentialService.cs
@@ -0,0 +1,8 @@
+﻿using Shark.Fido2.Domain;
+
+namespace Shark.Fido2.Sample.Blazor.Abstractions.Services;
+
+public interface ICredentialService
+{
+    Task<Credential?> Get(byte[] id, CancellationToken cancellationToken);
+}
