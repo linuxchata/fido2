970e504de6ae8034fc254f8c7ec837feae3778c0
linuxchata <linuxchata@hotmail.com>
2025-08-31 12:31:49 +0200

    Revert sample application to use in-memory storage

--- src/Shark.Fido2.Sample/Program.cs
+++ src/Shark.Fido2.Sample/Program.cs
@@ -5,7 +5,6 @@ using Shark.Fido2.Sample.Abstractions.Services;
 using Shark.Fido2.Sample.Middlewares;
 using Shark.Fido2.Sample.Services;
 using Shark.Fido2.Sample.Swagger;
-using Shark.Fido2.SqlServer;
 using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -77,7 +76,7 @@ if (!builder.Environment.IsDevelopment())
 }
 
 builder.Services.AddFido2(builder.Configuration);
-builder.Services.AddFido2SqlServer();
+builder.Services.AddFido2InMemoryStore();
 
 builder.Services.AddTransient<ILoginService, LoginService>();
 builder.Services.AddTransient<ICredentialService, CredentialService>();
