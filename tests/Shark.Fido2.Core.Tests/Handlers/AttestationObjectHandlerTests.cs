e3ea8e6d02b7cdd72a575a0a930a58442b202484
linuxchata <linuxchata@hotmail.com>
2025-09-20 09:14:43 +0200

    Update unit tests for attestation object handler

--- tests/Shark.Fido2.Core.Tests/Handlers/AttestationObjectHandlerTests.cs
+++ tests/Shark.Fido2.Core.Tests/Handlers/AttestationObjectHandlerTests.cs
@@ -13,9 +13,10 @@ namespace Shark.Fido2.Core.Tests.Handlers;
 [TestFixture]
 internal class AttestationObjectHandlerTests
 {
-    private AttestationObjectHandler _sut = null!;
     private IAuthenticatorDataParserService _authenticatorDataParserService = null!;
     private Mock<IAttestationObjectValidator> _attestationObjectValidatorMock = null!;
+
+    private AttestationObjectHandler _sut = null!;
 
     [SetUp]
     public void Setup()
@@ -38,8 +39,47 @@ internal class AttestationObjectHandlerTests
     }
 
     [Test]
-    public async Task Handle_WheniPhone8AttestationObjectValid_ThenReturnsValue()
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("   ")]
+    public async Task Handle_WhenAttestationObjectIsNullOrEmpty_ThenReturnsInternalResult(string? attestationObject)
     {
+        // Arrange
+        var creationOptions = PublicKeyCredentialCreationOptionsBuilder.Build();
+
+        // Act
+        var result = await _sut.Handle(
+            attestationObject!, ClientDataBuilder.BuildCreate(), creationOptions, CancellationToken.None);
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.HasError, Is.True);
+        Assert.That(result.Message, Is.EqualTo("Attestation object cannot be null"));
+        Assert.That(result.Value, Is.Null);
+    }
+
+    [Test]
+    public async Task Handle_WhenCreationOptionsAreNull_ThenReturnsInternalResult()
+    {
+        // Arrange
+        var attestationObject = "o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YViYSZYN5YgOjGh0NBcPZHZgW4/krrmihjLHmVzzuoMdl2NdAAAAAAAAAAAAAAAAAAAAAAAAAAAAFNIOIaOVgJRyI6ffE8tNV4tHvGJVpQECAyYgASFYIEgIOe/+LSvpyPB010CZ4+ox3EAG6dp611nzoff5QH15IlggC/DWA8k1rogu86PSgVzEjD9ObamYaO2dbj710ogx1dw=";
+
+        // Act
+        var result = await _sut.Handle(
+            attestationObject!, ClientDataBuilder.BuildCreate(), null!, CancellationToken.None);
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.HasError, Is.True);
+        Assert.That(result.Message, Is.EqualTo("Creation options cannot be null"));
+        Assert.That(result.Value, Is.Null);
+    }
+
+    [Test]
+    public async Task Handle_WhenAttestationObjectValidAndNoneAttestationFormat_ThenReturnsValue()
+    {
+        // Source: iPhone 8 authenticator.
+
         // Arrange
         var attestationObject = "o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YViYSZYN5YgOjGh0NBcPZHZgW4/krrmihjLHmVzzuoMdl2NdAAAAAAAAAAAAAAAAAAAAAAAAAAAAFNIOIaOVgJRyI6ffE8tNV4tHvGJVpQECAyYgASFYIEgIOe/+LSvpyPB010CZ4+ox3EAG6dp611nzoff5QH15IlggC/DWA8k1rogu86PSgVzEjD9ObamYaO2dbj710ogx1dw=";
         var creationOptions = PublicKeyCredentialCreationOptionsBuilder.Build();
@@ -56,8 +96,10 @@ internal class AttestationObjectHandlerTests
     }
 
     [Test]
-    public async Task Handle_WheniPhone14AttestationObjectValid_ThenReturnsValue()
+    public async Task Handle_WhenAttestationObjectValidAndNoneAttestationFormat2_ThenReturnsValue()
     {
+        // Source: iPhone 14 authenticator.
+
         // Arrange
         var attestationObject = "o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YViYSZYN5YgOjGh0NBcPZHZgW4/krrmihjLHmVzzuoMdl2NdAAAAAPv8MAcVTk7MjAtuAgVX170AFMM5L/63IyA9jqYyiZ9EaOwWhG40pQECAyYgASFYIHFhbupdvz+UhnJF4mph/jreh1RzjE7NDyl54kPLlbiLIlggdqxWZTTqfjbFSXBmGW0RWEJfvljHTKfB9upz0SBkPj8=";
         var creationOptions = PublicKeyCredentialCreationOptionsBuilder.Build();
@@ -74,8 +116,10 @@ internal class AttestationObjectHandlerTests
     }
 
     [Test]
-    public async Task Handle_WhenWindowsAttestationObjectValid_ThenReturnsValue()
+    public async Task Handle_WhenAttestationObjectValidAndPackedAttestationFormat_ThenReturnsValue()
     {
+        // Source: Windows 10 authenticator.
+
         // Arrange
         var attestationObject = "o2NmbXRmcGFja2VkZ2F0dFN0bXSiY2FsZzkBAGNzaWdZAQClP2a8p8lm+FUiGJAUj76ThUfAVUUWut6EVWUdZvC4/HBxyOCh3sZ15o+CgW4TA1dPYZpYJAx1f7AdK5JXJ7MEpgmIuVWTNklGSyWBI5FJWDgGg0LDzDFZqDuGFbupXPzWT9PP4/yBTOcAQ2ZM6YMe7o7ix95Ke9PZnyQ30oySbVyUINCQZTZucBJh9cGfb92na5I2iNEfd7JN80ea3g58xBjEol+jLAmkfPabTVa4PDuI3B7PtjV2AbpmFjB3yfq+PpScSTObjx9EqZ3EsSvEZHAfj9LwhMbEkBzDEfUxHt6xW9Vgqn32aV7VAKdkohTh5CUZNGFIC2CvKjeqFBWWaGF1dGhEYXRhWQFnSZYN5YgOjGh0NBcPZHZgW4/krrmihjLHmVzzuoMdl2NFAAAAAGAosBex1EwCtLOvza/Ja7IAIHgppX3fEq9YSztHkiwb17ns0+Px0i+cSd9aTkm1JD5LpAEDAzkBACBZAQCmBcYvuGi9gyjh5lXY0wiL0oYw1voBr5XHTwP+14ezQBR90zV93anRBAfqFr5MLzY+0EB+YhwjvhL51G0INgmFS6rUhpfG1wQp+MvSU7tSaK1MwZKB35r17oU77/zjroBt780iDHGdYaUx4UN0Mi4oIGe9pmZTTiSUOwq9KpoE4aixjVQNfurWUs036xnkFJ5ZMVON4ki8dXLuOtqgtNy06/X98EKsFcwNKA83ob6XKUZCnG2GlWQJyMBnE8p1p4k46r3DF5p6vdVH+3Ibujmcxhw/f6/M6UTvhvYofT+ljqFYhHKT2iRp1m2+iFQJAbcGCvXW9AWVWeqU1tBQ5yENIUMBAAE=";
         var creationOptions = PublicKeyCredentialCreationOptionsBuilder.Build();
