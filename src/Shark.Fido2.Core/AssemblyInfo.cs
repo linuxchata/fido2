5055085abff9a033281a96749d608e371f05da0f
linuxchata <linuxchata@hotmail.com>
2025-09-27 18:31:54 +0200

    Cleanup dependencies

--- src/Shark.Fido2.Core/AssemblyInfo.cs
+++ src/Shark.Fido2.Core/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Shark.Fido2.Core.Tests")]
-[assembly: InternalsVisibleTo("Shark.Fido2.Core.Integration.Tests")]
 [assembly: InternalsVisibleTo("Shark.Fido2.Core.Performance.Tests")]
