486ec376ec5c674eda90a69eb4e7a6050ac86c90
linuxchata <linuxchata@hotmail.com>
2025-11-07 20:28:29 +0100

    Add test cases for subject alternative name parser

--- tests/Shark.Fido2.Core.Tests/Services/SubjectAlternativeNameParserServiceTests.cs
+++ tests/Shark.Fido2.Core.Tests/Services/SubjectAlternativeNameParserServiceTests.cs
@@ -31,6 +31,25 @@ internal class SubjectAlternativeNameParserServiceTests
     }
 
     [Test]
+    [TestCase("Directory Address:TPMVersion=id:13 + TPMModel=NPCT6xx", "")]
+    [TestCase("Directory Address:TPMVersion=id:13 + TPMModel=NPCT6xx + TPMManufacturer=", "")]
+    [TestCase("Directory Address:TPMVersion=id:13 + TPMModel=NPCT6xx + TPMManufacturer=id:", "id:")]
+    public void Parse_WhenSubjectAlternativeNameHasNameNotationAndNoManufacturer_ThenReturnsTpmIssuer(
+        string subjectAlternativeName,
+        string expectedManufacturer)
+    {
+        // Act
+        var result = _sut.Parse(subjectAlternativeName);
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Manufacturer, Is.EqualTo(expectedManufacturer));
+        Assert.That(result.ManufacturerValue, Is.Empty);
+        Assert.That(result.Model, Is.EqualTo("NPCT6xx"));
+        Assert.That(result.Version, Is.EqualTo("id:13"));
+    }
+
+    [Test]
     public void Parse_WhenSubjectAlternativeNameHasNumericNotation_ThenReturnsTpmIssuer()
     {
         // Arrange
@@ -43,6 +62,25 @@ internal class SubjectAlternativeNameParserServiceTests
         Assert.That(result, Is.Not.Null);
         Assert.That(result.Manufacturer, Is.EqualTo("id:4E544300"));
         Assert.That(result.ManufacturerValue, Is.EqualTo("4E544300"));
+        Assert.That(result.Model, Is.EqualTo("NPCT6xx"));
+        Assert.That(result.Version, Is.EqualTo("id:13"));
+    }
+
+    [Test]
+    [TestCase("DirName:/2.23.133.2.3=id:13+2.23.133.2.2=NPCT6xx", "")]
+    [TestCase("DirName:/2.23.133.2.3=id:13+2.23.133.2.2=NPCT6xx+2.23.133.2.1=", "")]
+    [TestCase("DirName:/2.23.133.2.3=id:13+2.23.133.2.2=NPCT6xx+2.23.133.2.1=id:", "id:")]
+    public void Parse_WhenSubjectAlternativeNameHasNumericNotationAndNoManufacturer_ThenReturnsTpmIssuer(
+        string subjectAlternativeName,
+        string expectedManufacturer)
+    {
+        // Act
+        var result = _sut.Parse(subjectAlternativeName);
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Manufacturer, Is.EqualTo(expectedManufacturer));
+        Assert.That(result.ManufacturerValue, Is.Empty);
         Assert.That(result.Model, Is.EqualTo("NPCT6xx"));
         Assert.That(result.Version, Is.EqualTo("id:13"));
     }
