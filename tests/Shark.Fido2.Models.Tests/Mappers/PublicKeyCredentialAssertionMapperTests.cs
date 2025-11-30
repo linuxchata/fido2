6c80625e11f87244278929aab52cb742664d4012
LinuxChata <linuxchata@hotmail.com>
2025-11-30 12:04:31 +0100

    Add unit tests for PublicKeyCredentialAssertionMapper (#505)
    Add unit tests for PublicKeyCredentialAssertionMapper

--- tests/Shark.Fido2.Models.Tests/Mappers/PublicKeyCredentialAssertionMapperTests.cs
+++ tests/Shark.Fido2.Models.Tests/Mappers/PublicKeyCredentialAssertionMapperTests.cs
@@ -1,3 +1,4 @@
+using Shark.Fido2.Domain.Constants;
 using Shark.Fido2.Models.Mappers;
 using Shark.Fido2.Models.Responses;
 
@@ -25,7 +26,7 @@ internal class PublicKeyCredentialAssertionMapperTests
             Id = "Id",
             RawId = "RawId",
             Response = null!,
-            Type = "public-key",
+            Type = PublicKeyCredentialType.PublicKey,
         };
 
         // Act & Assert
@@ -47,7 +48,7 @@ internal class PublicKeyCredentialAssertionMapperTests
                 Signature = "Signature",
                 UserHandle = "UserHandler",
             },
-            Type = "public-key",
+            Type = PublicKeyCredentialType.PublicKey,
         };
 
         // Act
@@ -66,7 +67,7 @@ internal class PublicKeyCredentialAssertionMapperTests
     }
 
     [Test]
-    public void Map_WhenAssertionIsValidAndHasExtensions_ThenReturnsPublicKeyCredentialAssertion()
+    public void Map_WhenAssertionIsValidAndWithAppIdUserVerificationMethodLargeBlobExtensions_ThenReturnsPublicKeyCredentialAssertion()
     {
         // Arrange
         var assertion = new ServerPublicKeyCredentialAssertion
@@ -80,7 +81,7 @@ internal class PublicKeyCredentialAssertionMapperTests
                 Signature = "Signature",
                 UserHandle = "UserHandler",
             },
-            Type = "public-key",
+            Type = PublicKeyCredentialType.PublicKey,
             Extensions = new ServerAuthenticationExtensionsClientOutputs
             {
                 AppId = true,
@@ -97,11 +98,61 @@ internal class PublicKeyCredentialAssertionMapperTests
         var result = assertion.Map();
 
         // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Id, Is.EqualTo(assertion.Id));
+        Assert.That(result.RawId, Is.EqualTo(assertion.RawId));
+        Assert.That(result.Type, Is.EqualTo(assertion.Type));
+        Assert.That(result.Response.ClientDataJson, Is.EqualTo(assertion.Response.ClientDataJson));
+        Assert.That(result.Response.AuthenticatorData, Is.EqualTo(assertion.Response.AuthenticatorData));
+        Assert.That(result.Response.Signature, Is.EqualTo(assertion.Response.Signature));
+        Assert.That(result.Response.UserHandle, Is.EqualTo(assertion.Response.UserHandle));
         Assert.That(result.Extensions, Is.Not.Null);
         Assert.That(result.Extensions.AppId, Is.EqualTo(assertion.Extensions.AppId));
         Assert.That(result.Extensions.UserVerificationMethod, Is.EqualTo(assertion.Extensions.UserVerificationMethod));
         Assert.That(result.Extensions.LargeBlob, Is.Not.Null);
         Assert.That(result.Extensions.LargeBlob!.Blob, Is.EqualTo(assertion.Extensions.LargeBlob.Blob));
         Assert.That(result.Extensions.LargeBlob.Written, Is.EqualTo(assertion.Extensions.LargeBlob.Written));
+    }
+
+    [Test]
+    public void Map_WhenAssertionIsValidAndWithAppIdUserVerificationMethodExtensions_ThenReturnsPublicKeyCredentialAssertion()
+    {
+        // Arrange
+        var assertion = new ServerPublicKeyCredentialAssertion
+        {
+            Id = "Id",
+            RawId = "RawId",
+            Response = new ServerAuthenticatorAssertionResponse
+            {
+                ClientDataJson = "ClientDataJson",
+                AuthenticatorData = "AuthenticatorData",
+                Signature = "Signature",
+                UserHandle = "UserHandler",
+            },
+            Type = PublicKeyCredentialType.PublicKey,
+            Extensions = new ServerAuthenticationExtensionsClientOutputs
+            {
+                AppId = true,
+                UserVerificationMethod = [[1236]],
+                LargeBlob = null,
+            },
+        };
+
+        // Act
+        var result = assertion.Map();
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Id, Is.EqualTo(assertion.Id));
+        Assert.That(result.RawId, Is.EqualTo(assertion.RawId));
+        Assert.That(result.Type, Is.EqualTo(assertion.Type));
+        Assert.That(result.Response.ClientDataJson, Is.EqualTo(assertion.Response.ClientDataJson));
+        Assert.That(result.Response.AuthenticatorData, Is.EqualTo(assertion.Response.AuthenticatorData));
+        Assert.That(result.Response.Signature, Is.EqualTo(assertion.Response.Signature));
+        Assert.That(result.Response.UserHandle, Is.EqualTo(assertion.Response.UserHandle));
+        Assert.That(result.Extensions, Is.Not.Null);
+        Assert.That(result.Extensions.AppId, Is.EqualTo(assertion.Extensions.AppId));
+        Assert.That(result.Extensions.UserVerificationMethod, Is.EqualTo(assertion.Extensions.UserVerificationMethod));
+        Assert.That(result.Extensions.LargeBlob, Is.Null);
     }
 }
