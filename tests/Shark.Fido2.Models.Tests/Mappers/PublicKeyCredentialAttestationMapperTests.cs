755960d50a13d160b1ecb7fc26fe2f5c033caaa2
linuxchata <linuxchata@hotmail.com>
2025-12-06 20:24:01 +0100

    Add missing test case

--- tests/Shark.Fido2.Models.Tests/Mappers/PublicKeyCredentialAttestationMapperTests.cs
+++ tests/Shark.Fido2.Models.Tests/Mappers/PublicKeyCredentialAttestationMapperTests.cs
@@ -1,4 +1,5 @@
-﻿using Shark.Fido2.Models.Mappers;
+﻿using Shark.Fido2.Domain.Constants;
+using Shark.Fido2.Models.Mappers;
 using Shark.Fido2.Models.Responses;
 
 namespace Shark.Fido2.Models.Tests.Mappers;
@@ -25,7 +26,7 @@ internal class PublicKeyCredentialAttestationMapperTests
             Id = "Id",
             RawId = "RawId",
             Response = null!,
-            Type = "public-key",
+            Type = PublicKeyCredentialType.PublicKey,
         };
 
         // Act & Assert
@@ -45,7 +46,7 @@ internal class PublicKeyCredentialAttestationMapperTests
                 ClientDataJson = "ClientDataJson",
                 AttestationObject = "AttestationObject",
             },
-            Type = "public-key",
+            Type = PublicKeyCredentialType.PublicKey,
         };
 
         // Act
@@ -75,7 +76,7 @@ internal class PublicKeyCredentialAttestationMapperTests
                 AttestationObject = "AttestationObject",
                 Transports = ["usb", "nfc"],
             },
-            Type = "public-key",
+            Type = PublicKeyCredentialType.PublicKey,
             Extensions = new ServerAuthenticationExtensionsClientOutputs
             {
                 AppIdExclude = true,
@@ -125,7 +126,7 @@ internal class PublicKeyCredentialAttestationMapperTests
                 AttestationObject = "AttestationObject",
                 Transports = ["usb", "nfc"],
             },
-            Type = "public-key",
+            Type = PublicKeyCredentialType.PublicKey,
             Extensions = new ServerAuthenticationExtensionsClientOutputs
             {
                 AppIdExclude = true,
@@ -152,6 +153,47 @@ internal class PublicKeyCredentialAttestationMapperTests
         Assert.That(result.Extensions.UserVerificationMethod, Is.EqualTo(attestation.Extensions.UserVerificationMethod));
         Assert.That(result.Extensions.CredentialProperties, Is.Not.Null);
         Assert.That(result.Extensions.CredentialProperties.RequireResidentKey, Is.EqualTo(attestation.Extensions.CredentialProperties.RequireResidentKey));
+        Assert.That(result.Extensions.LargeBlob, Is.Null);
+    }
+
+    [Test]
+    public void Map_WhenAttestationIsValidAndAndWithAppIdExcludeUserVerificationMethodExtensions_ThenReturnsPublicKeyCredentialAttestation()
+    {
+        // Arrange
+        var attestation = new ServerPublicKeyCredentialAttestation
+        {
+            Id = "Id",
+            RawId = "RawId",
+            Response = new ServerAuthenticatorAttestationResponse
+            {
+                ClientDataJson = "ClientDataJson",
+                AttestationObject = "AttestationObject",
+                Transports = ["usb", "nfc"],
+            },
+            Type = PublicKeyCredentialType.PublicKey,
+            Extensions = new ServerAuthenticationExtensionsClientOutputs
+            {
+                AppIdExclude = true,
+                UserVerificationMethod = [[1236]],
+                CredentialProperties = null!,
+                LargeBlob = null!,
+            },
+        };
+
+        // Act
+        var result = attestation.Map();
+
+        // Assert
+        Assert.That(result.Id, Is.EqualTo(attestation.Id));
+        Assert.That(result.RawId, Is.EqualTo(attestation.RawId));
+        Assert.That(result.Response.ClientDataJson, Is.EqualTo(attestation.Response.ClientDataJson));
+        Assert.That(result.Response.AttestationObject, Is.EqualTo(attestation.Response.AttestationObject));
+        Assert.That(result.Type, Is.EqualTo(attestation.Type));
+        Assert.That(result.Extensions, Is.Not.Null);
+        Assert.That(result.Extensions.AppIdExclude, Is.EqualTo(attestation.Extensions.AppIdExclude));
+        Assert.That(result.Extensions.UserVerificationMethod, Is.EqualTo(attestation.Extensions.UserVerificationMethod));
+        Assert.That(result.Extensions.CredentialProperties, Is.Not.Null);
+        Assert.That(result.Extensions.CredentialProperties.RequireResidentKey, Is.Null);
         Assert.That(result.Extensions.LargeBlob, Is.Null);
     }
 }
