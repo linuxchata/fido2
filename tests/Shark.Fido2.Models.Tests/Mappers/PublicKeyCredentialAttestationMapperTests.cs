84f87376316cdc133d3f0bdba23dfc269f199761
linuxchata <linuxchata@hotmail.com>
2025-12-06 10:34:51 +0100

    Add unit tests for PublicKeyCredentialAttestationMapper

--- tests/Shark.Fido2.Models.Tests/Mappers/PublicKeyCredentialAttestationMapperTests.cs
+++ tests/Shark.Fido2.Models.Tests/Mappers/PublicKeyCredentialAttestationMapperTests.cs
@@ -62,7 +62,7 @@ internal class PublicKeyCredentialAttestationMapperTests
     }
 
     [Test]
-    public void Map_WhenAttestationIsValidAndHasExtensions_ThenReturnsPublicKeyCredentialAttestation()
+    public void Map_WhenAttestationIsValidAndAndWithAppIdExcludeUserVerificationMethodCredentialPropertiesLargeBlobExtensions_ThenReturnsPublicKeyCredentialAttestation()
     {
         // Arrange
         var attestation = new ServerPublicKeyCredentialAttestation
@@ -96,6 +96,11 @@ internal class PublicKeyCredentialAttestationMapperTests
         var result = attestation.Map();
 
         // Assert
+        Assert.That(result.Id, Is.EqualTo(attestation.Id));
+        Assert.That(result.RawId, Is.EqualTo(attestation.RawId));
+        Assert.That(result.Response.ClientDataJson, Is.EqualTo(attestation.Response.ClientDataJson));
+        Assert.That(result.Response.AttestationObject, Is.EqualTo(attestation.Response.AttestationObject));
+        Assert.That(result.Type, Is.EqualTo(attestation.Type));
         Assert.That(result.Extensions, Is.Not.Null);
         Assert.That(result.Extensions.AppIdExclude, Is.EqualTo(attestation.Extensions.AppIdExclude));
         Assert.That(result.Extensions.UserVerificationMethod, Is.EqualTo(attestation.Extensions.UserVerificationMethod));
@@ -104,5 +109,49 @@ internal class PublicKeyCredentialAttestationMapperTests
         Assert.That(result.Extensions.LargeBlob, Is.Not.Null);
         Assert.That(result.Extensions.LargeBlob!.Supported, Is.EqualTo(attestation.Extensions.LargeBlob.Supported));
         Assert.That(result.Extensions.LargeBlob.Blob, Is.EqualTo(attestation.Extensions.LargeBlob.Blob));
+    }
+
+    [Test]
+    public void Map_WhenAttestationIsValidAndAndWithAppIdExcludeUserVerificationMethodCredentialPropertiesExtensions_ThenReturnsPublicKeyCredentialAttestation()
+    {
+        // Arrange
+        var attestation = new ServerPublicKeyCredentialAttestation
+        {
+            Id = "Id",
+            RawId = "RawId",
+            Response = new ServerAuthenticatorAttestationResponse
+            {
+                ClientDataJson = "ClientDataJson",
+                AttestationObject = "AttestationObject",
+                Transports = ["usb", "nfc"],
+            },
+            Type = "public-key",
+            Extensions = new ServerAuthenticationExtensionsClientOutputs
+            {
+                AppIdExclude = true,
+                UserVerificationMethod = [[1236]],
+                CredentialProperties = new ServerCredentialPropertiesOutput
+                {
+                    RequireResidentKey = true,
+                },
+                LargeBlob = null!,
+            },
+        };
+
+        // Act
+        var result = attestation.Map();
+
+        // Assert
+        Assert.That(result.Id, Is.EqualTo(attestation.Id));
+        Assert.That(result.RawId, Is.EqualTo(attestation.RawId));
+        Assert.That(result.Response.ClientDataJson, Is.EqualTo(attestation.Response.ClientDataJson));
+        Assert.That(result.Response.AttestationObject, Is.EqualTo(attestation.Response.AttestationObject));
+        Assert.That(result.Type, Is.EqualTo(attestation.Type));
+        Assert.That(result.Extensions, Is.Not.Null);
+        Assert.That(result.Extensions.AppIdExclude, Is.EqualTo(attestation.Extensions.AppIdExclude));
+        Assert.That(result.Extensions.UserVerificationMethod, Is.EqualTo(attestation.Extensions.UserVerificationMethod));
+        Assert.That(result.Extensions.CredentialProperties, Is.Not.Null);
+        Assert.That(result.Extensions.CredentialProperties.RequireResidentKey, Is.EqualTo(attestation.Extensions.CredentialProperties.RequireResidentKey));
+        Assert.That(result.Extensions.LargeBlob, Is.Null);
     }
 }
