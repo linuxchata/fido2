d94c00a362dc1d98df70065a715862fa5ed987ca
linuxchata <linuxchata@hotmail.com>
2025-12-25 11:52:26 +0100

    Remove usage of jQuery

--- src/Shark.Fido2.VisualStudio.Template/wwwroot/js/site.js
+++ src/Shark.Fido2.VisualStudio.Template/wwwroot/js/site.js
@@ -1,48 +1,54 @@
-﻿$(function () {
-    $("#registration").on("click", function (event) {
-        const usernameInput = $("#username-registration");
-        const displayNameInput = $("#display-name-registration");
-        const errorMessageSpan = $("#error-message-registration");
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const registrationButton = document.getElementById("registration");
+    if (registrationButton) {
+        registrationButton.addEventListener("click", function (event) {
+            const usernameInput = document.getElementById("username-registration");
+            const displayNameInput = document.getElementById("display-name-registration");
+            const errorMessageSpan = document.getElementById("error-message-registration");
 
-        const username = usernameInput.val();
-        const displayName = displayNameInput.val();
+            const username = usernameInput.value;
+            const displayName = displayNameInput.value;
 
-        if (!isValidInput(username)) {
-            errorMessageSpan.text("Please input a username");
-            return;
-        }
+            if (!isValidInput(username)) {
+                errorMessageSpan.textContent = "Please input a username";
+                return;
+            }
 
-        if (!isValidInput(displayName)) {
-            errorMessageSpan.text("Please input a display name");
-            return;
-        }
+            if (!isValidInput(displayName)) {
+                errorMessageSpan.textContent = "Please input a display name";
+                return;
+            }
 
-        usernameInput.prop("readonly", true);
-        displayNameInput.prop("readonly", true);
-        errorMessageSpan.text("");
+            usernameInput.readOnly = true;
+            displayNameInput.readOnly = true;
+            errorMessageSpan.textContent = "";
 
-        const button = event.target.closest('button');
-        const previousInnerHtml = button.innerHTML;
-        disableButton(button);
+            const button = event.target.closest('button');
+            const previousInnerHtml = button.innerHTML;
+            disableButton(button);
 
-        registrationOfDiscoverableCredential(username, displayName)
-            .finally(() => {
-                usernameInput.prop("readonly", false);
-                displayNameInput.prop("readonly", false);
-                enableButton(button, previousInnerHtml);
-            });
-    });
+            registration(username, displayName)
+                .finally(() => {
+                    usernameInput.readOnly = false;
+                    displayNameInput.readOnly = false;
+                    enableButton(button, previousInnerHtml);
+                });
+        });
+    }
 
-    $("#authentication").on("click", function (event) {
-        const button = event.target.closest('button');
-        const previousInnerHtml = button.innerHTML;
-        disableButton(button);
+    const authenticationButton = document.getElementById("authentication");
+    if (authenticationButton) {
+        authenticationButton.addEventListener("click", function (event) {
+            const button = event.target.closest('button');
+            const previousInnerHtml = button.innerHTML;
+            disableButton(button);
 
-        authenticationWithDiscoverableCredential()
-            .finally(() => {
-                enableButton(button, previousInnerHtml);
-            });
-    });
+            authentication()
+                .finally(() => {
+                    enableButton(button, previousInnerHtml);
+                });
+        });
+    }
 });
 
 function isValidInput(value) {
