a3ce51fe5df8d3e79d63b7cb8c6056390ac7378f
linuxchata <linuxchata@hotmail.com>
2026-04-16 08:33:55 +0200

    Improve naming

--- src/Shark.Fido2.ConvenienceMetadata.Core/Abstractions/IConvenienceMetadataCachedService.cs
+++ src/Shark.Fido2.ConvenienceMetadata.Core/Abstractions/IConvenienceMetadataCachedService.cs
@@ -12,6 +12,6 @@ public interface IConvenienceMetadataCachedService
     /// </summary>
     /// <param name="aaguid">The AAGUID of the authenticator.</param>
     /// <param name="cancellationToken">A cancellation token.</param>
-    /// <returns>The cached metadata payload item or null if not found.</returns>
+    /// <returns>The cached convenience metadata payload item or null if not found.</returns>
     Task<ConvenienceMetadataPayloadItem?> Get(Guid aaguid, CancellationToken cancellationToken);
 }
