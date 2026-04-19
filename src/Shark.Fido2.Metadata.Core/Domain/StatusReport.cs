2c537e573b3f8b406f0d24ad797952e8f4da6a2b
LinuxChata <linuxchata@hotmail.com>
2026-04-19 11:53:21 +0200

    Update XML comments for MDS domain models (#550)
    Update XML comments for MDS domain models

--- src/Shark.Fido2.Metadata.Core/Domain/StatusReport.cs
+++ src/Shark.Fido2.Metadata.Core/Domain/StatusReport.cs
@@ -1,8 +1,17 @@
-﻿namespace Shark.Fido2.Metadata.Core.Domain;
+namespace Shark.Fido2.Metadata.Core.Domain;
 
+/// <summary>
+/// Represents a status report for an authenticator.
+/// </summary>
 public sealed class StatusReport
 {
+    /// <summary>
+    /// Gets the date when the status became effective.
+    /// </summary>
     public string? EffectiveDate { get; init; }
 
+    /// <summary>
+    /// Gets the status of the authenticator.
+    /// </summary>
     public required string Status { get; init; }
 }
