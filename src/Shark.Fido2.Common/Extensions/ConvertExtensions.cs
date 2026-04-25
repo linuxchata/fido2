b71e9ce876891440cf15a355f038497ff716ab33
LinuxChata <linuxchata@hotmail.com>
2026-04-25 09:58:38 +0200

    Update identity model packages (#571)
    Update identity model packages

--- src/Shark.Fido2.Common/Extensions/ConvertExtensions.cs
+++ src/Shark.Fido2.Common/Extensions/ConvertExtensions.cs
@@ -24,14 +24,42 @@ public static class ConvertExtensions
 
     public static bool IsBase64Url(this string base64Url)
     {
-        try
-        {
-            Base64UrlEncoder.Decode(base64Url);
-            return true;
-        }
-        catch
+        if (string.IsNullOrEmpty(base64Url))
         {
             return false;
         }
+
+        var span = base64Url.AsSpan().TrimEnd('=');
+        var remainder = span.Length % 4;
+
+        if (remainder == 1)
+        {
+            return false;
+        }
+
+        foreach (var c in span)
+        {
+            if (!char.IsAsciiLetterOrDigit(c) && c != '-' && c != '_')
+            {
+                return false;
+            }
+        }
+
+        return remainder switch
+        {
+            2 => (GetBase64UrlValue(span[^1]) & 0xF) == 0,
+            3 => (GetBase64UrlValue(span[^1]) & 0x3) == 0,
+            _ => true
+        };
     }
+
+    private static int GetBase64UrlValue(char c) => c switch
+    {
+        >= 'A' and <= 'Z' => c - 'A',
+        >= 'a' and <= 'z' => c - 'a' + 26,
+        >= '0' and <= '9' => c - '0' + 52,
+        '-' => 62,
+        '_' => 63,
+        _ => -1
+    };
 }
