c7ff4d7dd6644d53073d8b1be60627d8c1286b3d
LinuxChata <linuxchata@hotmail.com>
2026-06-27 22:31:26 +0200

    Improve VS template project (#599)
    Improve VS template project

---
+++ templates/projects/Shark.Fido2.VisualStudio.Template/content/PublicKeyAlgorithms.cs
@@ -0,0 +1,25 @@
+﻿namespace Shark.Fido2.Sample.VisualStudio.Template;
+
+public static class PublicKeyAlgorithms
+{
+    private static readonly Dictionary<int, string> Algorithms = new()
+    {
+        { -7, "ES256 (ECDSA w/ SHA-256)" },
+        { -8, "EdDSA (EdDSA)" },
+        { -35, "ES384 (ECDSA w/ SHA-384)" },
+        { -36, "ES512 (ECDSA w/ SHA-512)" },
+        { -37, "PS256 (RSASSA-PSS w/ SHA-256)" },
+        { -38, "PS384 (RSASSA-PSS w/ SHA-384)" },
+        { -39, "PS512 (RSASSA-PSS w/ SHA-512)" },
+        { -47, "ES256K (ECDSA using secp256k1 curve and SHA-256)" },
+        { -257, "RS256 (RSASSA-PKCS1-v1_5 using SHA-256)" },
+        { -258, "RS384 (RSASSA-PKCS1-v1_5 using SHA-384)" },
+        { -259, "RS512 (RSASSA-PKCS1-v1_5 using SHA-512)" },
+        { -65535, "RS1 (RSASSA-PKCS1-v1_5 using SHA-1)" },
+    };
+
+    public static string Get(int key)
+    {
+        return Algorithms.FirstOrDefault(x => x.Key == key).Value ?? "Unknown";
+    }
+}
